3c584fda70201bec9a46ca5197ed222cd63a947e
Diego Ugarte <diego.ugarte@outlook.com>
2023-10-05 23:04:33 +0900

    Add source files

---
+++ src/parsers.jl
@@ -0,0 +1,13 @@
+###############################################################################
+#                                   File IO                                   #
+###############################################################################
+
+include("parsers/parser_toml.jl")
+include("parsers/parser_pdb.jl")
+include("parsers/parser_cif.jl")
+include("parsers/parser_seq.jl")
+include("parsers/parser_top.jl")
+include("parsers/parser_crd.jl")
+include("parsers/parser_dcd.jl")
+include("parsers/selection.jl")
+
